7d6c84531a5940c3b483d8e7335a5b463deccb59
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-02-26 17:19:00 +0100

    add grid world editor dev project

---
+++ WorldDesignTest/Assets/CodeSmile/Scripts/Editor/GridPathDrawerEditor.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Scripts
+{
+	[CustomEditor(typeof(GridPathDrawer))]
+	public class GridPathDrawerEditor : Editor
+	{
+		private void OnSceneGUI()
+		{
+			var pathDrawer = (GridPathDrawer)target;
+			var path = pathDrawer.PathPoints.ToArray();
+			Handles.DrawAAPolyLine(path);
+			
+			Handles.color = Handles.elementPreselectionColor;
+			var pos = pathDrawer.transform.position;
+			var size = pathDrawer.GridSettings.TileSize.x / 5f;
+			for (int i = 0; i < path.Length; i++)
+			{
+				Handles.CylinderHandleCap(i, path[i], Quaternion.Euler(new Vector3(90f,0f,0f)), size, EventType.Repaint);
+			}
+		}
+	}
+}
---
+++ WorldDesignTest/Assets/CodeSmile/Scripts/GridPathDrawer.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeSmile.Scripts
+{
+	[ExecuteInEditMode]
+	public class GridPathDrawer : MonoBehaviour
+	{
+		[SerializeField] private List<Vector3> m_PathPoints = new();
+		[SerializeField] private Grid m_GridSettings = new() { TileSize = new Vector2Int(30, 30) };
+
+		private void Awake() => ResetPath();
+
+		public List<Vector3> PathPoints => m_PathPoints;
+		public Grid GridSettings => m_GridSettings;
+
+		public void ResetPath()
+		{
+			Debug.Log("ResetPath");
+			m_PathPoints.Clear();
+			m_PathPoints.Add(Vector3.left * m_GridSettings.TileSize.x);
+			m_PathPoints.Add(Vector3.right * m_GridSettings.TileSize.x);
+			m_PathPoints.Add(Vector3.right * m_GridSettings.TileSize.x + Vector3.forward * m_GridSettings.TileSize.y);
+			m_PathPoints.Add(Vector3.left * m_GridSettings.TileSize.x + Vector3.forward * m_GridSettings.TileSize.y);
+		}
+
+		public struct Grid
+		{
+			public Vector2Int TileSize;
+		}
+	}
+}
