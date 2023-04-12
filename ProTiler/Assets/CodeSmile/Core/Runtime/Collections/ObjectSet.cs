d6f17d8a1aee4d8583fc9c9fb7435bb1c07c5c73
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-12 12:43:44 +0200

    finish objectset and tests

--- ProTiler/Assets/CodeSmile/Core/Runtime/Collections/ObjectSet.cs
+++ ProTiler/Assets/CodeSmile/Core/Runtime/Collections/ObjectSet.cs
@@ -3,44 +3,82 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Object = UnityEngine.Object;
 
-namespace CodeSmile.ProTiler.Collections
+namespace CodeSmile.Collections
 {
 	/// <summary>
-	/// Contains UnityEngine.Object instances so they can be accessed by the same index in both editor and runtime,
-	/// because GetInstanceID changes between Editor and Runtime.
+	///     Contains UnityEngine.Object instances so they can be accessed by the same index in both editor and runtime.
+	///     Fault tolerant: Index out of bounds access returns a default object rather than throwing exceptions.
+	///     Note: GetInstanceID cannot be used as an index because its value may differ between Editor and Runtime.
 	/// </summary>
 	[Serializable]
 	public class ObjectSet<T> where T : Object
 	{
-		private List<T> m_Objects;
+		public T DefaultObject;
+		private int m_NextIndex;
+		private Dictionary<int, T> m_IndexedObjects = new();
+		//private HashSet<ObjectIndexPair> m_ObjectIndexPairs;
 
 		public T this[int index]
 		{
-			get
-			{
-				if (index < 0 || index >= m_Objects.Count)
-					return null;
-
-				return m_Objects[index];
-			}
+			get => m_IndexedObjects.TryGetValue(index, out var existingObject) ? existingObject : DefaultObject;
+			set => m_IndexedObjects[index] = Contains(index) ? value : throw new KeyNotFoundException($"index {index} does not exist in set");
 		}
-		public int Count => m_Objects.Count;
 
-		public ObjectSet() => m_Objects = new List<T>();
+		public int Count => m_IndexedObjects.Count;
 
-		public int AddOrGetExisting(T obj)
+		public ObjectSet()
+			: this(null) {}
+
+		public ObjectSet(T defaultObject) => DefaultObject = defaultObject;
+
+		public bool Add(T item) => Add(item, out var _);
+
+		public bool Add(T item, out int index)
 		{
-			if (obj == null)
-				throw new ArgumentNullException("obj");
+			if (Contains(item))
+			{
+				index = m_IndexedObjects.FirstOrDefault(x => x.Value == item).Key;
+				return false;
+			}
 
-			var existingIndex = m_Objects.FindIndex(o => o.GetInstanceID() == obj.GetInstanceID());
-			if (existingIndex >= 0)
-				return existingIndex;
-
-			m_Objects.Add(obj);
-			return m_Objects.Count - 1;
+			m_IndexedObjects.Add(m_NextIndex, item);
+			index = m_NextIndex;
+			m_NextIndex++;
+			return true;
 		}
+
+		public void Clear() => m_IndexedObjects.Clear();
+
+		public bool Contains(int index) => m_IndexedObjects.ContainsKey(index);
+		public bool Contains(T item) => m_IndexedObjects.ContainsValue(item);
+
+		public bool Remove(T item)
+		{
+			foreach (var kvp in m_IndexedObjects)
+			{
+				if (kvp.Value.Equals(item))
+				{
+					m_IndexedObjects.Remove(kvp.Key);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool Remove(int index) => m_IndexedObjects.Remove(index);
+
+		/*
+		private sealed class ObjectIndexPair : IEquatable<T>
+		{
+			public T Object;
+			public int Index;
+			public bool Equals(T other) => EqualityComparer<T>.Default.Equals(Object, other);
+			public override bool Equals(object obj) => obj is T other && Equals(other);
+			public override int GetHashCode() => Object != null ? Object.GetHashCode() : 0;
+		}
+		*/
 	}
 }
---
+++ ProTiler/Assets/CodeSmile/Core/Tests/Editor/ObjectSetTests.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using CodeSmile.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace CodeSmile.Editor.Tests
+{
+	public class ObjectSetTests
+	{
+		private GameObject m_DefaultGO;
+		private GameObject m_GO1;
+		private GameObject m_GO2;
+		private GameObject m_GO3;
+
+		private ObjectSet<GameObject> m_Set;
+		private ObjectSet<GameObject> m_FilledSet;
+
+		[SetUp]
+		public void SetUp()
+		{
+			m_DefaultGO = new GameObject("DEFAULT");
+			m_GO1 = new GameObject("one");
+			m_GO2 = new GameObject("two");
+			m_GO3 = new GameObject("three");
+			m_Set = new ObjectSet<GameObject>(m_DefaultGO);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Object.DestroyImmediate(m_DefaultGO);
+			Object.DestroyImmediate(m_GO1);
+			Object.DestroyImmediate(m_GO2);
+			Object.DestroyImmediate(m_GO3);
+		}
+
+		[Test]
+		public void CreateTests()
+		{
+			var set = new ObjectSet<GameObject>();
+			Assert.AreEqual(null, set.DefaultObject);
+			Assert.AreEqual(0, set.Count);
+
+			set = new ObjectSet<GameObject>(m_DefaultGO);
+			Assert.AreEqual(m_DefaultGO, set.DefaultObject);
+			Assert.AreEqual(0, set.Count);
+		}
+
+		[Test]
+		public void AddAndRemoveTests()
+		{
+			var set = m_Set;
+
+			var added = set.Add(m_GO1);
+			Assert.AreEqual(true, added);
+			Assert.AreEqual(1, set.Count);
+			added = set.Add(m_GO1);
+			Assert.IsFalse(added);
+			Assert.AreEqual(1, set.Count);
+			Assert.IsTrue(set.Contains(m_GO1));
+			Assert.IsFalse(set.Contains(m_GO2));
+
+			added = set.Add(m_GO2, out var index2);
+			Assert.IsTrue(added);
+			Assert.AreEqual(2, set.Count);
+			Assert.AreEqual(1, index2);
+			added = set.Add(m_GO2, out index2);
+			Assert.IsFalse(added);
+			Assert.AreEqual(2, set.Count);
+			Assert.AreEqual(1, index2);
+			Assert.IsTrue(set.Contains(m_GO1));
+			Assert.IsTrue(set.Contains(m_GO2));
+
+			var didRemove = set.Remove(m_GO1);
+			Assert.IsTrue(didRemove);
+			Assert.AreEqual(1, set.Count);
+			Assert.IsFalse(set.Contains(m_GO1));
+			Assert.IsTrue(set.Contains(m_GO2));
+
+			didRemove = set.Remove(m_GO3);
+			Assert.IsFalse(didRemove);
+			set.Add(m_GO3, out var index3);
+			Assert.AreEqual(2, set.Count);
+			Assert.AreEqual(2, index3);
+
+			set.Remove(1);
+			Assert.AreEqual(1, set.Count);
+			Assert.IsFalse(set.Contains(m_GO1));
+			Assert.IsFalse(set.Contains(m_GO2));
+			Assert.IsTrue(set.Contains(m_GO3));
+
+			set.Clear();
+			Assert.AreEqual(0, set.Count);
+			Assert.IsFalse(set.Contains(m_GO3));
+		}
+
+		[Test]
+		public void IndexerTests()
+		{
+			var set = m_Set;
+
+			var obj = set[0];
+			Assert.AreEqual(set.DefaultObject, obj);
+			Assert.Throws(typeof(KeyNotFoundException), () => { set[0] = m_GO1; });
+
+			set.Add(m_GO1);
+			set.Add(m_GO2);
+			set.Add(m_GO3);
+			Assert.AreEqual(m_GO1, set[0]);
+			Assert.AreEqual(m_GO2, set[1]);
+			Assert.AreEqual(m_GO3, set[2]);
+			set[2] = m_GO1;
+			Assert.AreEqual(m_GO1, set[2]);
+		}
+	}
+}
