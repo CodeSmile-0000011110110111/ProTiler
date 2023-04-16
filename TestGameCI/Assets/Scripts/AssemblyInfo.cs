b5f995a82d7c38622e6e670f6dd9d31c4eaaa953
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-16 12:07:44 +0200

    cleanup repo

--- TestGameCI/Assets/Scripts/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) 2021-2023 Steffen Itterheim
-// Refer to included LICENSE file for terms and conditions.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("EditorTests")]
-[assembly: InternalsVisibleTo("RuntimeTests")]
