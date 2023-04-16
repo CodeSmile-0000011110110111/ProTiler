093339ac956b1e15da220fc177644e9ab2791000
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-16 09:23:20 +0200

    split tests

---
+++ WorkflowBuilder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkflowBuilder
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			// process command line args
+			// load template file (test + build, test only)
+			// for each project
+			// for each platform
+			// for each unity version
+			// replace project name, platform, unity version
+			// write output file with filename project-platform-unityversion
+		}
+	}
+}
