26dfa9b6f38bfcfe72f08986f4bc3faa9b6627dd
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-17 17:31:22 +0200

    remove old & prototype code

--- ProTiler/Assets/CodeSmile/z Prototyping/Scripts/MiscTests/AutoDisablePickability.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[ExecuteInEditMode]
-public class AutoDisablePickability : MonoBehaviour
-{
-#if UNITY_EDITOR
-	private void OnEnable() => UnityEditor.SceneVisibilityManager.instance.DisablePicking(gameObject, true);
-#endif
-}
