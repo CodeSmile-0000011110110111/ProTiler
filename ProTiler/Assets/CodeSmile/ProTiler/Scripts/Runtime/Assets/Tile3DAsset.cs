779a23e9626042e39142e36d2db89dbd92729ec1
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-22 17:16:10 +0200

    fix build error

--- ProTiler/Assets/CodeSmile/ProTiler/Scripts/Runtime/Assets/Tile3DAsset.cs
+++ ProTiler/Assets/CodeSmile/ProTiler/Scripts/Runtime/Assets/Tile3DAsset.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 
 namespace CodeSmile.ProTiler.Assets
 {
+#if UNITY_EDITOR
 	[CreateAssetMenu(fileName = "New 3D Tile", menuName = Names.TileEditor + "/3D Tile", order = Menus.CreateAssetRootOrder)]
+#endif
 
 	public class Tile3DAsset : Tile3DAssetBase
 	{
 		//[MenuItem("GameObject/" + Names.TileEditor + "/" + Names.Tilemap3DMenu + "/" + RectangularTilemapMenuText, priority = Menus.CreateGameObjectPriority + 0)]
-
+#if UNITY_EDITOR
 		[MenuItem(Names.TileEditor + "/test create tile")]
+#endif
 		public static Tile3DAsset CreateInstance() => CreateInstance<Tile3DAsset>();
 	}
 }
