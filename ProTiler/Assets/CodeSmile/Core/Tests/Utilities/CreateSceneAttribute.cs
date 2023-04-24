c8679283b5ce6c5f5e6fd3a2a5f7bcdbf62bb42c
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-04-24 16:04:17 +0200

    keep open test scene if it's the same

--- ProTiler/Assets/CodeSmile/Core/Tests/Utilities/CreateSceneAttribute.cs
+++ ProTiler/Assets/CodeSmile/Core/Tests/Utilities/CreateSceneAttribute.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (C) 2021-2023 Steffen Itterheim
 // Refer to included LICENSE file for terms and conditions.
 
+using CodeSmile.Extensions;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System;
@@ -9,34 +10,44 @@ using System.IO;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace CodeSmile.ProTiler.Tests.Utilities
 {
 	/// <summary>
-	/// Creates a new empty scene for a unit test method.
+	///     Creates a new empty scene for a unit test method.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Method)]
 	public class EmptySceneAttribute : CreateSceneAttribute
 	{
 		/// <summary>
-		/// Creates a new empty scene for a unit test method.
+		///     Creates a new empty scene for a unit test method.
 		/// </summary>
-		/// <param name="scenePath">if non-empty, the scene will be saved as an asset for tests that verify correctness of save/load of a scene's contents. The saved scene asset is deleted after the test ran.</param>
+		/// <param name="scenePath">
+		///     if non-empty, the scene will be saved as an asset for tests that verify correctness of
+		///     save/load of a scene's contents. The saved scene asset is deleted after the test ran.
+		/// </param>
 		public EmptySceneAttribute(string scenePath = null)
 			: base(scenePath) {}
 	}
 
 	/// <summary>
-	/// Creates a new default scene (with Camera + Direct Light) for a unit test method.
+	///     Creates a new default scene (with Camera + Direct Light) for a unit test method.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Method)]
 	public class DefaultSceneAttribute : CreateSceneAttribute
 	{
 		/// <summary>
-		/// Creates a new default scene (with Camera + Direct Light) for a unit test method.
+		///     Creates a new default scene (with Camera + Direct Light) for a unit test method.
+		///     Note: the scene contents will be restored with the exception of the default game objects "Main Camera" and
+		///     "Directional Light".
+		///     Any changes to these two objects will persist between tests.
 		/// </summary>
-		/// <param name="scenePath">if non-empty, the scene will be saved as an asset for tests that verify correctness of save/load of a scene's contents. The saved scene asset is deleted after the test ran.</param>
+		/// <param name="scenePath">
+		///     if non-empty, the scene will be saved as an asset for tests that verify correctness of
+		///     save/load of a scene's contents. The saved scene asset is deleted after the test ran.
+		/// </param>
 		public DefaultSceneAttribute(string scenePath = null)
 			: base(scenePath, NewSceneSetup.DefaultGameObjects) {}
 	}
@@ -51,22 +62,39 @@ namespace CodeSmile.ProTiler.Tests.Utilities
 			m_ScenePath = string.IsNullOrWhiteSpace(scenePath) == false ? Defines.TestAssetsPath + scenePath : null;
 			m_Setup = setup;
 
-			if (m_ScenePath != null)
-				CreateDirectoryIfNotExists();
+			CreateScenePathDirectoryIfNeeded();
 		}
 
 		IEnumerator IOuterUnityTestAction.BeforeTest(ITest test)
 		{
-			var scene = EditorSceneManager.NewScene(m_Setup);
+			var activeScene = SceneManager.GetActiveScene();
+			var sceneName = GetSceneName();
+			if (activeScene.name != sceneName)
+			{
+				var scene = EditorSceneManager.NewScene(m_Setup);
+				scene.name = sceneName;
 
-			if (m_ScenePath != null)
-				EditorSceneManager.SaveScene(scene, m_ScenePath);
+				if (m_ScenePath != null)
+					EditorSceneManager.SaveScene(scene, m_ScenePath);
+			}
 
 			yield return null;
 		}
 
 		IEnumerator IOuterUnityTestAction.AfterTest(ITest test)
 		{
+			var activeScene = SceneManager.GetActiveScene();
+			foreach (var rootGameObject in activeScene.GetRootGameObjects())
+			{
+				if (m_Setup == NewSceneSetup.DefaultGameObjects)
+				{
+					if (rootGameObject.name == "Main Camera" || rootGameObject.name == "Directional Light")
+						continue;
+				}
+
+				rootGameObject.DestroyInAnyMode();
+			}
+
 			if (m_ScenePath != null)
 			{
 				if (AssetDatabase.DeleteAsset(m_ScenePath) != true)
@@ -76,8 +104,17 @@ namespace CodeSmile.ProTiler.Tests.Utilities
 			yield return null;
 		}
 
-		private void CreateDirectoryIfNotExists()
+		private string GetSceneName()
 		{
+			var name = m_ScenePath != null ? Path.GetFileName(m_ScenePath) : m_Setup.ToString();
+			return $"Test [CreateScene] {name}";
+		}
+
+		private void CreateScenePathDirectoryIfNeeded()
+		{
+			if (m_ScenePath == null)
+				return;
+
 			var path = Application.dataPath.Replace("/Assets", "") + m_ScenePath;
 			path = Path.GetFullPath(path);
 			if (Directory.Exists(path) == false)
