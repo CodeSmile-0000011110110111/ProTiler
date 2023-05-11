78c7a39c03c05da11dcde20e9e3f044c79a2a7c6
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-05-11 11:35:27 +0200

    try updating script define symbols again - it's apparently not set for github actions

--- ProTiler/Assets/CodeSmile/ProTiler/Runtime/Data/CreatePropertyAttribute.cs
+++ ProTiler/Assets/CodeSmile/ProTiler/Runtime/Data/CreatePropertyAttribute.cs
@@ -2,14 +2,10 @@
 // Refer to included LICENSE file for terms and conditions.
 
 using System;
-using UnityEngine;
 
 namespace CodeSmile.ProTiler.Data
 {
 #if !UNITY_PROPERTIES_EXISTS
-	internal class CreatePropertyAttribute : Attribute
-	{
-		internal CreatePropertyAttribute() => Debug.Log("use of internal CreateProperty attribute");
-	}
+	internal class CreatePropertyAttribute : Attribute {}
 #endif
 }
